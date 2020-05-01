0796c44320020f2a7a80fa2961f08672e73d0c63
chundo <contact.nassa@gmail.com>
2020-05-01 12:14:32 -0700

    2020-05-1 9:26

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,26 +1,67 @@
 <style type="text/css" media="screen">
-    .container {
-        margin: 10px auto;
-        max-width: 600px;
-        text-align: center;
+    .notfound .notfound-404 h1 {
+
+        font-family: 'Montserrat', sans-serif;
+        font-size: 146px;
+        font-weight: 700;
+        margin: 0px;
+        color: #232323;
     }
 
-    h1 {
-        margin: 30px 0;
-        font-size: 4em;
-        line-height: 1;
-        letter-spacing: -1px;
+    .notfound h2 {
+        font-family: 'Montserrat', sans-serif;
+        font-size: 22px;
+        font-weight: 700;
+        margin: 0;
+        text-transform: uppercase;
+        color: #232323;
     }
+
+    .notfound p {
+        font-family: 'Montserrat', sans-serif;
+        color: #787878;
+        padding-bottom: 10px;
+        line-height: 1.5;
+        font-size: 16px;
+        font-weight: 300;
+    }
+
+    notfound a {
+        font-family: 'Montserrat', sans-serif;
+        display: inline-block;
+        padding: 12px 30px;
+        font-weight: 700;
+        background-color: #f99827;
+        color: #fff;
+        border-radius: 40px;
+        text-decoration: none;
+        -webkit-transition: 0.2s all;
+        transition: 0.2s all;
+    }
+
 </style>
 
-<div class="container">
-    <h1>404</h1>
-    <div class="error-content">
-        <h3><i class="fa fa-warning text-yellow"></i><strong>Page not found :(</strong></h3>
-        <p>
-            We could not find the page you were looking for.
-            You should maybe <a href="{{ url('/') }}">return to the dashboard</a>.
-        </p>
+{{--<div class="container">--}}
+{{--    <h1>404</h1>--}}
+{{--    <div class="error-content">--}}
+{{--        <h3><i class="fa fa-warning text-yellow"></i><strong>Page not found :(</strong></h3>--}}
+{{--        <p>--}}
+{{--            We could not find the page you were looking for.--}}
+{{--            You should maybe <a href="{{ url('/') }}">return to the dashboard</a>.--}}
+{{--        </p>--}}
 
+{{--    </div>--}}
+{{--</div>--}}
+<div class="container">
+    <div id="notfound">
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1>404</h1>
+            </div>
+            <h2>Oops! Page Not Be Found</h2>
+            <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily
+                unavailable</p>
+            <a href="http://uploadpot.com">Back to homepage</a>
+        </div>
     </div>
 </div>
