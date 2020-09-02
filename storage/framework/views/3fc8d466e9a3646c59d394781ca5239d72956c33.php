9326929ac28b9c544cae49e17bea8fdc058556e7
contact.nassa <contact.nassa@gmail.com>
2020-09-02 20:36:37 +0300

    2020-09-02 9:27

--- storage/framework/views/3fc8d466e9a3646c59d394781ca5239d72956c33.php
+++ storage/framework/views/3fc8d466e9a3646c59d394781ca5239d72956c33.php
@@ -1,6 +1,4 @@
-@extends('emails.master')
-
-@section('content')
+<?php $__env->startSection('content'); ?>
     <tr>
         <td bgcolor="#F4F5F5" style="border-collapse: collapse;">&nbsp;</td>
     </tr>
@@ -12,7 +10,7 @@
                     <td class="logo"
                         style="border-collapse: collapse; vertical-align: middle; padding-left:34px; padding-top:20px; padding-bottom:12px"
                         valign="middle">
-                        <img src="{{ isset($message) ? $message->embed(public_path('images/fidel-logo.png')) : 'cid:fidel-logo.png'  }}"
+                        <img src="<?php echo e(isset($message) ? $message->embed(public_path('images/fidel-logo.png')) : 'cid:fidel-logo.png'); ?>"
                              alt="Fidel"/>
                     </td>
                 </tr>
@@ -22,31 +20,33 @@
     <tr>
         <td class="content" style="border-collapse: collapse;">
             <div style="font-size: 18px; margin: 42px 40px 42px; padding: 0;">
-                @yield('body')
+                <?php echo $__env->yieldContent('body'); ?>
             </div>
         </td>
     </tr>
-@stop
+<?php $__env->stopSection(); ?>
 
-@section('footer')
+<?php $__env->startSection('footer'); ?>
     <p style="color: #A7A6A6; font-size: 13px; line-height: 18px; margin: 0 0 7px; padding: 0;">
-        <a href="{{ SOCIAL_LINK_FACEBOOK }}"
+        <a href="<?php echo e(SOCIAL_LINK_FACEBOOK); ?>"
            style="color: #A7A6A6; text-decoration: none; font-weight: bold; font-size: 10px;"><img
-                    src="{{ isset($message) ? $message->embed(public_path('images/emails/icon-facebook.png')) : 'cid:icon-facebook.png' }}"
+                    src="<?php echo e(isset($message) ? $message->embed(public_path('images/emails/icon-facebook.png')) : 'cid:icon-facebook.png'); ?>"
                     alt="Facebook"/></a>
-        <a href="{{ SOCIAL_LINK_TWITTER }}"
+        <a href="<?php echo e(SOCIAL_LINK_TWITTER); ?>"
            style="color: #A7A6A6; text-decoration: none; font-weight: bold; font-size: 10px;"><img
-                    src="{{ isset($message) ? $message->embed(public_path('images/emails/icon-twitter.png')) : 'cid:icon-twitter.png' }}"
+                    src="<?php echo e(isset($message) ? $message->embed(public_path('images/emails/icon-twitter.png')) : 'cid:icon-twitter.png'); ?>"
                     alt="Twitter"/></a>
-        <a href="{{ SOCIAL_LINK_GITHUB }}"
+        <a href="<?php echo e(SOCIAL_LINK_GITHUB); ?>"
            style="color: #A7A6A6; text-decoration: none; font-weight: bold; font-size: 10px;"><img
-                    src="{{ isset($message) ? $message->embed(public_path('images/emails/icon-github.png')) : 'cid:icon-github.png' }}"
+                    src="<?php echo e(isset($message) ? $message->embed(public_path('images/emails/icon-github.png')) : 'cid:icon-github.png'); ?>"
                     alt="GitHub"/></a>
     </p>
 
     <p style="color: #A7A6A6; font-size: 13px; line-height: 18px; margin: 0 0 7px; padding: 0;">
-        &#9400; {{ date('Y') }} | <?php echo trans('texts.app_name'); ?><br/>
-        <strong><a href="{{ URL::to('/settings/notifications') }}"
-                   style="color: #A7A6A6; text-decoration: none; font-weight: bold; font-size: 10px;">{{ strtoupper(trans('texts.email_preferences')) }}</a></strong>
+        &#9400; <?php echo e(date('Y')); ?> | <?php echo trans('texts.app_name'); ?><br/>
+        <strong><a href="<?php echo e(URL::to('/settings/notifications')); ?>"
+                   style="color: #A7A6A6; text-decoration: none; font-weight: bold; font-size: 10px;"><?php echo e(strtoupper(trans('texts.email_preferences'))); ?></a></strong>
     </p>
-@stop
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('emails.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
